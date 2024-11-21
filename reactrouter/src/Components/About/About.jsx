622a86838946a2ee0d5cc173b877ec0d41168807
saurabhsonalkar <sonalkarsaurabh9@gmail.com>
2024-11-21 10:49:15 +0530

    modified

--- reactrouter/src/Components/About/About.jsx
+++ reactrouter/src/Components/About/About.jsx
@@ -1,11 +1,11 @@
 export default function About() {
     return (
-        <div className="py-16 bg-white">
+        <div className="py-1 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-20164.jpg?t=st=1732162683~exp=1732166283~hmac=e033fe1fad900989d55c9ec052aea6ce0327223407ef19ebd3595ede32836cb3&w=740"
                             alt="image"
                         />
                     </div>
--- reactrouter/src/Components/Github/Github.jsx
+++ reactrouter/src/Components/Github/Github.jsx
@@ -1,23 +1,31 @@
-
+import { useEffect, useState } from "react"
 
 
 function Github(){
+    const [avatar, setAvatar] = useState('')
+    const [followers, setFollowers] = useState(0)
     
+    useEffect(()=>{
+        const fetchAvatar = async() => {
+            const response = await fetch("https://api.github.com/users/saurabhsonalkar")
+            const data = await response.json()
 
-    const avtar = () => {
-        const data = fetch("https://api.github.com/users/saurabhsonalkar")
-        .then((res)=>res.json())
+            setAvatar(data.avatar_url)
+            setFollowers(data.followers)
+        }
 
-        const pic = data.avtar
-        return pic
-    }
+        fetchAvatar()
+    },[])
 
-    return(
-        <>
-         
-        
-        </>
-    )
+return(
+    <>
+    <div className="text-3xl text-center bg-orange-500 p-4 ">Followers: 1{followers}M</div>
+    <div><img src={avatar} alt="" className="sm:w-96 w-48"/></div>
+    
+    </>
+)
+
+
 }
 
 export default Github
