8d6eb37c5b9243550c0f8b98911b978df953b30f
saurabhsonalkar <sonalkarsaurabh9@gmail.com>
2024-11-30 12:27:06 +0530

    industry componenets

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from '../components/container/Container'
+import Logo from '../components/Logo'
+import LogoutBtn from '../components/Header/LogoutBtn'
 
-export  {Header,Footer};
+export  {Header,Footer, Container,Logo,LogoutBtn};
